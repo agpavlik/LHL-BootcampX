a9a5f9c8d9043ace132f0211b990306eeb052785
agpavlik <agpavlik@gmail.com>
2023-05-01 23:27:17 +0000

    Add folder additional queries and folder 2_queries_joins

---
+++ 2_queries_joins/time_cohort_spent_on_assignment.sql
@@ -0,0 +1,5 @@
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = assignment_submissions.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = 'FEB12';
